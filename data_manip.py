b98c9c830fe20f85311c63f489e259339c64db7f
Kyle Lawrence <alawrence815@gmail.com>
2024-04-20 13:26:22 -0400

    Updated file naming for data manip function

--- data_manip.py
+++ data_manip.py
@@ -1,14 +1,10 @@
-import pydub as pd
-from pydub import utils, AudioSegment
+from pydub import AudioSegment
+import os
 
 
 def audio_manip(fname):
-    file_name = fname
+    file_name = os.path.basename(fname)
     audio = AudioSegment.from_wav(file_name)
     mono_chan = audio.set_channels(1)
-    meta_data = pd.utils.mediainfo(r"C:\Users\alawr\PycharmProjects\Sound project\spidam-project\Clap1_mono_chan.wav")
-    print(meta_data)
-    mono_chan.export(f"{file_name}_mono_chan.wav", format="wav")
-
-
-audio_manip("Clap2.wav")
+    file = os.path.splitext(file_name)
+    mono_chan.export(f"{file[0]}_mono_chan.wav", format="wav")
